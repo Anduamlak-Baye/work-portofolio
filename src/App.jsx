83a4acf06bf0a8d4b713a4dba85d65ce5a3db2d3
Anduamlak <andutok808@gmail.com>
2025-10-28 22:40:25 +0300

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   useEffect(() => {
